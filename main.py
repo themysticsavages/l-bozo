66af764a62d270ee7ecc323e09282864b0cf4ddc
ajskateboarder <adijoshi08@gmail.com>
2024-08-05 15:37:52 -0400

    dfsoihsdfiofhd

--- main.py
+++ main.py
@@ -5,43 +5,80 @@ from ssl import SSLError
 from operator import itemgetter
 import threading
 import asyncio
+from sqlitedict import SqliteDict
 
 import requests
 import websocket
-from dotenv import load_dotenv
+from dotenv import dotenv_values
 from nextcord.ext import commands
 from nextcord.webhook import SyncWebhook
-from nextcord import Intents, Message, File
+from nextcord import Intents, Message, File, Embed
 
 logging.basicConfig()
 
 intents = Intents.default()
 intents.message_content = True
+intents.typing = True
 
-MEOWER_TOKEN, DSC_TOKEN, CHANNEL, WEBHOOK, MEOWER_USR, DSC_USR = itemgetter(
-    "MEOWER_TOKEN", "DSC_TOKEN", "CHANNEL", "WEBHOOK", "MEOWER_USR", "DSC_USR"
-)(load_dotenv())
+MEOWER_TOKEN, DSC_TOKEN, CHANNEL, WEBHOOK, MEOWER_USR, DSC_PING, DSC_USR = itemgetter(
+    "MEOWER_TOKEN",
+    "DSC_TOKEN",
+    "CHANNEL",
+    "WEBHOOK",
+    "MEOWER_USR",
+    "DSC_PING",
+    "DSC_USR",
+)(dotenv_values())
 CHANNEL = int(CHANNEL)
 
 bot = commands.Bot(command_prefix="$", intents=intents)
-d2m = {}
+db = SqliteDict("posts.db")
+
+
+@bot.event
+async def on_typing(_channel, user, _when):
+    if user == DSC_USR:
+        h = requests.post(
+            "https://api.meower.org/home/typing", headers={"Token": MEOWER_TOKEN}
+        )
 
 
 @bot.event
 async def on_message(message: Message):
-    reply_to = []
     if message.webhook_id or message.channel.id != CHANNEL:
         return
-    if message.reference:
-        reply_to.append(d2m[message.reference.message_id])
 
-    with requests.get(
+    reply_to = [db[message.reference.message_id]] if message.reference else []
+    attachments = []
+
+    for a in message.attachments:
+        fh = BytesIO(await a.read())
+        files = {"file": (a.filename, fh, a.content_type)}
+        resp = requests.post(
+            "https://uploads.meower.org/attachments",
+            files=files,
+            headers={
+                "Authorization": MEOWER_TOKEN,
+            },
+        )
+        if resp.status_code != 200:
+            print(resp.status_code, resp.text)
+            print("could not send attachment ", a.filename)
+            continue
+        attachments.append(resp.json()["id"])
+
+    resp = requests.post(
         "https://api.meower.org/home",
-        json={"attachments": [], "content": message.content, "reply_to": reply_to},
+        json={
+            "attachments": attachments,
+            "content": message.content,
+            "reply_to": reply_to,
+        },
         headers={"Token": MEOWER_TOKEN, "Content-Type": "application/json"},
-    ) as resp:
-        if resp.status != 200:
-            print("could not send")
+    )
+    if resp.status_code != 200:
+        print("could not send")
+        print(resp.status_code, resp.text)
 
 
 def get_attachment(a):
@@ -63,18 +100,23 @@ async def send_webhook_post(packet):
 
     replies = packet["val"]["reply_to"]
     if replies:
-        reply_id = {v: k for k, v in d2m.items()}[replies[0]["_id"]]
+        reply_id = {v: k for k, v in db.items()}[replies[0]["_id"]]
         old_msg = h.fetch_message(reply_id)
-        content = (
-            f"> @{old_msg.author.name} {replies[0]['p']} {old_msg.jump_url}\n{content}"
+        embed = Embed(description=f"{replies[0]['p']} [⤴]({old_msg.jump_url})")
+        embed.set_author(
+            name=f"@{old_msg.author.name}",
+            url=f"https://app.meower.org/users/{old_msg.author.name}",
+            icon_url=old_msg.author.display_avatar.url,
         )
+        params["embed"] = embed
 
     message = h.send(content.replace(MEOWER_USR, DSC_USR), **params)
-    d2m[message.id] = packet["val"]["_id"]
+    db[message.id] = packet["val"]["_id"]
+    db.commit()
 
 
 async def edit_webhook_post(packet, delete):
-    message_id = {v: k for k, v in d2m.items()}[
+    message_id = {v: k for k, v in db.items()}[
         packet["val"]["_id"] if not delete else packet["val"]["post_id"]
     ]
 
